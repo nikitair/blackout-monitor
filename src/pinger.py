0973f95e859234940e80c3d55a477fdc7319ef99
Nikita Stoliarov <stlrvnk@gmail.com>
2024-09-01 15:12:21 +0300

    ping address as a method

--- src/pinger.py
+++ src/pinger.py
@@ -11,7 +11,7 @@ load_dotenv()
 class Pinger:
     
     
-    def __init__(self, location_id: int):
+    def __init__(self, location_id: int) -> None:
         self.location_id = location_id
         self.ip_address = None
         self.state = None
@@ -19,7 +19,7 @@ class Pinger:
         
         
     @class_connector
-    def set_location_data(self, con):
+    def set_location_data(self, con) -> None:
         with con.cursor() as cur:
             cur.execute(
                 f"""
@@ -46,36 +46,36 @@ class Pinger:
                 self.state_changed_at = data[0][3] if data[0][3] else datetime.now(timezone.utc)
 
 
-def ping_address(ip_address: str) -> dict | None:
-    logger.info(f"ping address - ({ip_address})")
-    try:
-        response = subprocess.run(
-            ["ping", "-c", "1", ip_address],
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE
-        )
-        
-        status_code = response.returncode
-        message = response.stdout.decode()
-        error = response.stderr.decode()
-        
-        logger.info(f"ping status code - ({status_code})")
-        logger.info(f"ping message - ({message})")
-        
-        if status_code != 0:
-            if not error:
-                logger.warning(f"!!! blackout")
+    def ping_address(self) -> dict | None:
+        logger.info(f"ping address - ({self.ip_address})")
+        try:
+            response = subprocess.run(
+                ["ping", "-c", "1", self.ip_address],
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE
+            )
+            
+            status_code = response.returncode
+            message = response.stdout.decode()
+            error = response.stderr.decode()
+            
+            logger.info(f"ping status code - ({status_code})")
+            logger.info(f"ping message - ({message})")
+            
+            if status_code != 0:
+                if not error:
+                    logger.warning(f"!!! blackout")
+                else:
+                    logger.error(f"! error - ({error})")
             else:
-                logger.error(f"! error - ({error})")
-        else:
-            logger.info("success")
+                logger.info("success")
 
-        return {
-            "blackout": status_code != 0,
-            "status_code": status_code,
-            "message": message,
-            "error": error
-        }
-    except Exception as ex:
-        logger.error(f"!!! exception occurred - ({ex})")
+            return {
+                "blackout": status_code != 0,
+                "status_code": status_code,
+                "message": message,
+                "error": error
+            }
+        except Exception as ex:
+            logger.error(f"!!! exception occurred - ({ex})")
 
