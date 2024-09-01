5ac3e2cac8dd8ecda791919cf1c7e59327d3342c
Nikita Stoliarov <stlrvnk@gmail.com>
2024-09-01 13:37:03 +0300

    pinger function

--- src/main.py
+++ src/main.py
@@ -1,11 +1,15 @@
 import os
+import time
 from dotenv import load_dotenv
-from pinger import ping_router
+from pinger import ping_address
 
 load_dotenv()
 
+os.environ['TZ'] = 'UTC'
+time.tzset()
+
 IP_ADDRESS = os.getenv("IP_ADDRESS", "")
 
-ping_response = ping_router(IP_ADDRESS)
+ping_response = ping_address(IP_ADDRESS)
 
-print(ping_response.stdout)
+print(ping_response)
--- src/pinger.py
+++ src/pinger.py
@@ -6,19 +6,39 @@ from logging_config import logger
 load_dotenv()
 
 
-
-def ping_router(ip_address: str):
-    logger.info(f"ping address - {ip_address}")
+def ping_address(ip_address: str) -> dict | None:
+    logger.info(f"ping address - ({ip_address})")
     try:
-        ping_response = subprocess.run(
+        response = subprocess.run(
             ["ping", "-c", "1", ip_address],
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE
         )
-        return ping_response
+        
+        status_code = response.returncode
+        message = response.stdout.decode()
+        error = response.stderr.decode()
+        
+        logger.info(f"ping status code - ({status_code})")
+        logger.info(f"ping message - ({message})")
+        
+        if status_code != 0:
+            if not error:
+                logger.warning(f"!!! blackout")
+            else:
+                logger.error(f"! error - ({error})")
+        else:
+            logger.info("success")
+
+        return {
+            "blackout": status_code != 0,
+            "status_code": status_code,
+            "message": message,
+            "error": error
+        }
     except Exception as ex:
-        print(ex)
+        logger.error(f"!!! error occurred - ({ex})")
 
 
 if __name__ == "__main__":
-    res = ping_router(os.getenv("IP_ADDRESS"))
+    res = ping_address(os.getenv("IP_ADDRESS"))
