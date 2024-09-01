8c5208163aceddea7997355469225a919774f432
Nikita Stoliarov <stlrvnk@gmail.com>
2024-09-01 20:01:11 +0300

    no exceptions handling

--- src/server.py
+++ src/server.py
@@ -18,32 +18,32 @@ def index():
 
 @app.post("/ping")
 async def ping(request: PingRequest):
-    try:
-        ip = request.ip
-        logger.info(f"ping ip - ({ip})")
+    # try:
+    ip = request.ip
+    logger.info(f"ping ip - ({ip})")
+    
+    ping_response = subprocess.run(
+        ["ping", "-c", "1", ip],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE
+    )
+    
+    status_code = ping_response.returncode
+    message = ping_response.stdout.decode()
+    error = ping_response.stderr.decode()
+    
+    logger.info(f"ping status code - ({status_code})")
+    logger.info(f"ping message - ({message})")
+    logger.info(f"ping error - ({error})")
+    
+    return {
+        "status_code": status_code,
+        "message": message,
+        "error": error
+    }
         
-        ping_response = subprocess.run(
-            ["ping", "-c", "1", ip],
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE
-        )
-        
-        status_code = ping_response.returncode
-        message = ping_response.stdout.decode()
-        error = ping_response.stderr.decode()
-        
-        logger.info(f"ping status code - ({status_code})")
-        logger.info(f"ping message - ({message})")
-        logger.info(f"ping error - ({error})")
-        
-        return {
-            "status_code": status_code,
-            "message": message,
-            "error": error
-        }
-        
-    except Exception:
-        return {"message": "Bad Data received"}
+    # except Exception:
+    #     return {"message": "Bad Data received"}
 
 
 if __name__ == "__main__":
